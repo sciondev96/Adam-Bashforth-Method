f63a6250b1fc89cef3ccef8b007ab8bd0232bafb
Satvik <49191921+sciondev96@users.noreply.github.com>
2019-04-02 16:05:47 +0530

    Update Evaluate.java

--- Evaluate.java
+++ Evaluate.java
@@ -1,3 +1,4 @@
+//USED TO EVALUATE THE POSTFIX EXPRESSION RECEIVED FROM Convert.java
 import java.lang.Math;
 class Stack1
 {
@@ -20,7 +21,7 @@ class Stack1
 }
 class Evaluate
 {
-   public double calculate(String s,double x,double y)
+   public double calculate(String s,double x,double y)       //used to accept x & y values for which y' value has to be calculated
    {
      int n;
      double r=0;
@@ -31,9 +32,9 @@ class Evaluate
        char ch=s.charAt(i);
        if(ch>='0'&&ch<='9')
          a.push((int)(ch-'0'));
-       else if(ch==(int)'x')
+       else if(ch==(int)'x')           //used to replace the variable x in the equation with the value of x
     	 a.push(x);
-       else if(ch==(int)'y')
+       else if(ch==(int)'y')           //used to replace the variable y in the equation with the value of y
     	 a.push(y);
        else
        {
